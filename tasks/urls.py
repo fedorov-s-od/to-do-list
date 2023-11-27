1d456f43f6be99c21c3e0d320aa3e86075bb6a5a
Serhii Fedorov <fedorov.od.ua@gmail.com>
2023-11-27 12:56:42 +0200

    Update views & urls files, registration & index pages, creating superuser

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 
 from .views import (
     index,
-    TagListView
+    TagListView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+    task_change_status
 )
 
 urlpatterns = [
@@ -11,6 +15,14 @@ urlpatterns = [
         "tags/",
         TagListView.as_view(),
         name="tag-list",
+    ),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path(
+        "tasks/<int:pk>/taskchangestatus/",
+        task_change_status,
+        name="task-change-status",
     ),
 ]
 
--- tasks/views.py
+++ tasks/views.py
@@ -1,17 +1,60 @@
 from django.shortcuts import render
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.http import HttpResponseRedirect
 
 from .models import Task, Tag
 
 
+@login_required
 def index(request):
     """View function for the home page of the site."""
+    post_list = Task.objects.all().order_by('-created_at')
+    paginator = Paginator(post_list, 5)  # Paginate by 5 items per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
-    return render(request, "tasks/index.html", context={})
+    context = {
+        "post_list": page_obj
+    }
+
+    return render(request, "tasks/index.html", context=context)
 
 
-class TagListView(generic.ListView):
+class TagListView(LoginRequiredMixin, generic.ListView):
     model = Tag
     context_object_name = "tag_list"
     template_name = "tasks/tag_list.html"
     paginate_by = 5
+
+
+class TaskCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:task-list")
+
+
+class TaskUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:task-list")
+
+
+class TaskDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("tasks:task-list")
+
+
+@login_required
+def task_change_status(request, pk):
+    task = Task.objects.get(id=pk)
+    if task.is_done:
+        task.is_done = False
+    else:
+        task.is_done = True
+    task.save()
+
+    return HttpResponseRedirect(reverse_lazy("tasks:task-list"))
